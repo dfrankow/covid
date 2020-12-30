e1b1034600ec2efbe99edcbb861de12dbf688e16
Dan Frankowski <dfrankow@gmail.com>
2020-12-30 11:50:44 -0600

    Add vaccines administered

--- mn/refresh.sh
+++ mn/refresh.sh
@@ -7,8 +7,11 @@ wget -O data/wcrmap.csv 'https://www.health.state.mn.us/diseases/coronavirus/sta
 # They detect bots, so I have to download by hand at the URL
 # wget -O data/HospitalCapacity_HistoricCSV_tcm1148-449110.csv https://mn.gov/covid19/assets/HospitalCapacity_HistoricCSV_tcm1148-449110.csv
 
+
+
 source ../.venv/bin/activate
 cat situation.html | parse_html_tables.py
+cat vaccine_admin.html | parse_html_tables.py
 mv *.tsv data
 Rscript -e 'rmarkdown::render("covid.Rmd")'
 
