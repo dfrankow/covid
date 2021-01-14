e55eb188c440ef5a399a7429dbc6c82b4e0fc741
Dan Frankowski <dfrankow@gmail.com>
2021-01-14 11:47:03 -0600

    New vaccine file

--- mn/refresh.sh
+++ mn/refresh.sh
@@ -7,6 +7,9 @@ wget -O data/wcrmap.csv 'https://www.health.state.mn.us/diseases/coronavirus/sta
 # They detect bots, so I have to download by hand at the URL
 # wget -O data/HospitalCapacity_HistoricCSV_tcm1148-449110.csv https://mn.gov/covid19/assets/HospitalCapacity_HistoricCSV_tcm1148-449110.csv
 
+# They detect bots, so I have to download by hand at the URL
+# wget -O data/vaxadminbyweek.csv https://mn.gov/covid19/assets/Doses%20Administered%20By%20Week_tcm1148-462844.csv
+
 wget -O vaccine_admin.html https://www.health.state.mn.us/diseases/coronavirus/vaccine/stats/admin.html
 
 
@@ -20,6 +23,7 @@ Rscript -e 'rmarkdown::render("covid.Rmd")'
 the_date=`date +"%Y%m%d"`
 
 cp data/wcrmap.csv data/wcrmap.${the_date}.csv
+# cp data/vaxadminbyweek.csv data/vaxadminbyweek.${the_date}.csv
 mv covid.pdf covid.${the_date}.pdf
 mv situation.html situation.html.${the_date}
 mv vaccine_admin.html vaccine_admin.html.${the_date}
